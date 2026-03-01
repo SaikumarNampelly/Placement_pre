7035b120c6db32008b06c84a5525e41ab0209957
Chintu41 <saikumarnampelly31@gmail.com>
2026-03-01 23:17:32 +0530

    feat: integrate Tailwind CSS and add new Day-2 components
    - Added Tailwind CSS as a dependency and configured it in the project.
    - Created new components for Day-2: UserList (API fetching), Counter, and Livesearch.
    - Updated App component to include new Day-2 components and styled with Tailwind CSS.
    - Implemented user fetching from an API in UserList with loading and error handling.
    - Developed a Counter component with increment/decrement functionality and history tracking.
    - Added a Livesearch component with real-time search functionality for a list of fruits.

--- Tasks/src/App.jsx
+++ Tasks/src/App.jsx
@@ -1,24 +1,29 @@
 import Product from "./Day-1/Props/product.jsx"
 import ToggleDetails from "./Day-1/Toggle/ToggleDetails.jsx"
 import UserDashboard from "./Day-1/UserDashboard/User.jsx"
+import UserList from "./Day-2/Api-fetching.jsx"
+import Counter from "./Day-2/Counter.jsx"
+import Livesearch from "./Day-2/Livesearch.jsx"
 
 const App = () => {
   return (
     <div>
-      <div>
+      {/* <div>
+        <h1>Day-1</h1>
         <h2>Task-1</h2>
       <Product/>
-      </div>
-
-      <div>
         <h2>Task-2</h2>
       <ToggleDetails/>
-      </div>
-      <div>
         <h2>task-3</h2>
       <UserDashboard/>
+      </div> */}
+      <div>
+        <h1 className="text-3xl font-extrabold text-center mb-8">Day-2</h1>
+        {/* <Counter/>
+        <UserList/> */}
+        <Livesearch/> 
       </div>
     </div>
   )
 }
-export default App    
+export default App
